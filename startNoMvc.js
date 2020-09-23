b288f4aa9fd50159ca17abf2bb0029466ba84322
Jordi Pan <302624262@student.rocmondriaan.nl>
2020-09-23 13:58:58 +0200

    aaa

--- startNoMvc.js
+++ startNoMvc.js
@@ -1,8 +1,38 @@
-vakjes = new Array(7);
+let vakjes = new Array(6);
+for (let i = 0; i < 6; i++) {
+    vakjes[i] = new Array(7);
+}
 
-for (i = 0; i < 7; i++) {
-    console.log(i);
-    vakjes[i] = new Array(8);
+let el = document.querySelector(".container");
+el.addEventListener('click' , event => {
+    handleClick(event);
+})
+
+function handleClick(event)
+{
+    let teller=0;
+    console.log(event);
+    const vakjesHTML=document.querySelectorAll('.container > div');
+    vakjesHTML.forEach(element => {
+        if(element===event.target)
+        {
+            console.log(teller%7);
+        }
+        teller++;
+    })
 }
 
 
+/*function test() {
+
+    id = 0;
+
+    for (i = 0; i < 1; i++) {
+        el[i] = id;
+        console.log(el);
+        id++;
+    }
+}
+
+test();
+*/
