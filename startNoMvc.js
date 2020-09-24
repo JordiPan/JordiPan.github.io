f86bcffdb710a875c99128437e9e655578769255
Jordi Pan <302624262@student.rocmondriaan.nl>
2020-09-24 15:06:29 +0200

    fisches plaatsen

--- startNoMvc.js
+++ startNoMvc.js
@@ -1,9 +1,17 @@
-let vakjes = new Array(6);
-for (let i = 0; i < 6; i++) {
-    vakjes[i] = new Array(7);
+let vakjes=new Array(6);
+for (let i=0;i<6;i++)
+{
+    vakjes[i]=new Array(7);
 }
 
+for(let rij=0;rij<6;rij++)
+    for(let kolom=0;kolom<7;kolom++)
+        vakjes[rij][kolom]="leeg";
+
+
 let el = document.querySelector(".container");
+let message = document.querySelector("#messageWindow");
+
 el.addEventListener('click' , event => {
     handleClick(event);
 })
@@ -11,16 +19,63 @@ el.addEventListener('click' , event => {
 function handleClick(event)
 {
     let teller=0;
-    console.log(event);
     const vakjesHTML=document.querySelectorAll('.container > div');
     vakjesHTML.forEach(element => {
         if(element===event.target)
         {
-            console.log(teller%7);
+            element.style.backgroundColor = "red";
+            message.innerHTML = "Je klikte op nummer: " + teller;
+            plaatsFiche(teller%7);
+
+            toonModel();
         }
         teller++;
     })
 }
+
+let container = document.getElementById("cont");
+for(var i = 0; i < 42; i++){
+    var backside = document.createElement("div");
+    backside.className = 'items';
+    container.appendChild(backside);
+}
+
+
+function plaatsFiche(kolom)
+{
+    for(let rij=5;rij>=0;rij--)
+    {
+        if(vakjes[rij][kolom]==="leeg")
+        {
+            vakjes[rij][kolom]="vol";
+            return true;
+        }
+    }
+    return false;
+}
+
+function toonModel()
+{
+    let teller=0;
+    console.log(vakjes);
+    const vakjesHTML=document.querySelectorAll('.container > div');
+    vakjesHTML.forEach(element=>{
+        element.style.backgroundColor = "white";
+    })
+
+    vakjesHTML.forEach(element => {
+
+        let rij=Math.floor(teller/7);
+        let kolom=teller%7;
+        console.log(rij + ' ' + kolom +vakjes[rij][kolom]);
+        if(vakjes[rij][kolom]=="vol"){
+            element.style.backgroundColor = "red";
+            console.log("hoi");
+        }
+        teller++;
+    })
+}
+
 
 
 /*function test() {
