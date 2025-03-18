6734203fb62039b79c6c0feb05cc65000f797a29
JordiPan <jordipan.jp@gmail.com>
2025-03-18 21:05:17 +0100

    start with a few changes in file placement

---
+++ client-side/controller/Controller.js
@@ -0,0 +1,104 @@
+import {Model} from "../model/Model.js";
+import {View} from "../view/View.js";
+export class Controller {
+    constructor() {
+       
+        this.model = new Model();
+        this.view = new View();
+        //op dit moment geen idee over bind this
+        this.view.bindMakeBoard(this.handleStart.bind(this))
+        this.view.bindStop(this.handleStop.bind(this))
+        this.view.bindPlaceChip(this.handlePlacing.bind(this))
+        this.view.bindRematch(this.handleRematch.bind(this))
+    }
+
+    async handleStart(event) {
+        // event.preventDefault();
+        if (this.model.checkDuplicateNames(this.view.getNames())) {
+            alert('Namen mogen niet hetzelfde zijn/AI mag niet dezelfde naam hebben als speler');
+            return;
+        }
+        event.preventDefault();
+        this.model.makeModelBoard();
+        this.model.setgameMode(this.view.getGamemode());
+
+        if(this.model.getGameMode() == 'ai') {
+            this.model.setDifficulty(this.view.getDifficulty());
+
+        }
+
+        this.model.setNames(this.view.getNames());
+        this.model.decideFirst();
+
+        this.view.toggleTitle();
+        this.view.setNames(this.model.getNames());
+        this.view.makeBoard(event);
+        this.view.hideStartWindow();
+        await this.view.decideFirst(this.model.getNames(), this.model.getTurnName());
+
+        await this.checkForAiMove();
+        // this.view.updateTurn(this.model.getTurnName());
+    }
+
+    handleStop() {
+        this.model.resetTurnCount();
+        this.model.resetWins();
+        this.model.resetNames();
+
+        this.view.resetNames();
+        this.view.hidePlayingField();
+        this.view.toggleTitle();
+    }
+    
+    handlePlacing(placementLocation) {
+        let placement = this.model.placeModelChip(placementLocation);
+
+        //kolom is vol (misschien shake animatie toevoegen)
+        if(!placement) {
+            return;
+        }
+        this.view.placeChip(this.model.getPlacement(), this.model.getColor());
+        
+        let gamestate = this.model.checkWinner(placement);
+
+        if(gamestate === 1) {
+            this.view.endGame(null);
+            return;
+        }
+        
+        if(gamestate === 2) {
+            let winnerName = this.model.getTurnName();
+            let stats = this.model.updateWins(winnerName);
+            this.view.updateWins(stats);
+            this.view.endGame(winnerName);
+            return;
+        }
+        this.changeTurns();
+    }
+    async handleRematch(event) {
+        this.model.resetTurnCount();
+        this.model.makeModelBoard();
+        this.model.decideFirst();
+
+        this.view.makeBoard(event);
+        this.view.hideResultsWindow();
+        await this.view.decideFirst(this.model.getNames(), this.model.getTurnName());
+        
+        this.checkForAiMove();
+        // this.view.updateTurn(this.model.getTurnName());
+    }
+    changeTurns() {
+        this.model.switchTurn();
+        this.view.updateTurn(this.model.getTurnName());
+        
+        this.checkForAiMove();
+    }
+    async checkForAiMove() {
+        if(this.model.isAiTurn()) {
+            this.view.toggleInteractivity();
+            let aiMove = await this.model.getAiMove();
+            this.view.toggleInteractivity();
+            this.handlePlacing(aiMove);
+        }
+    }
+}   
---
+++ client-side/view/View.js
@@ -0,0 +1,159 @@
+export class View {
+  constructor() {
+    this.title = document.getElementById("title");
+    this.turn = document.getElementById("turn");
+    this.winsScore1 = document.getElementById("win1");
+    this.winsScore2 = document.getElementById("win2");
+    this.end = document.querySelector("#result-window");
+    this.board = document.getElementById("board");
+    this.playingField = document.querySelector(".margin-container");
+    this.scoreName1 = document.getElementById("player-name-one");
+    this.scoreName2 = document.getElementById("player-name-two");
+    this.spaces = document.getElementsByClassName("item");
+    this.winner = document.getElementById("winner");
+    this.rematch = document.getElementById("rematch");
+    this.start = document.getElementById("start-window");
+    this.stop = document.getElementById("stop");
+    this.statBlock1 = document.getElementById("shadow-one");
+    this.statBlock2 = document.getElementById("shadow-two");
+    this.formName1 = document.getElementById("player1-name");
+    this.formName2 = document.getElementById("player2-name");
+    this.gamemode = document.getElementById("gamemode");
+    this.difficulty = document.getElementById("difficulty");
+  }
+  // prevent(event) {
+  //   event.preventDefault();
+  // }
+  bindMakeBoard(handler) {
+    this.start.addEventListener("submit", handler);
+  }
+  bindStop(handler) {
+    this.stop.addEventListener("click", handler);
+  }
+  bindPlaceChip(handler) {
+    //reageert niet als er op bord randen wordt geklikt
+    this.board.addEventListener("click", (event) => {
+      if (event.target && event.target.classList.contains("item")) {
+        //geeft kolom mee ipv volgorde nummer van div
+        handler(event.target.id % 7);
+      }
+    });
+    // this.board.addEventListener('click', handler);
+  }
+  bindRematch(handler) {
+    this.rematch.addEventListener("click", handler);
+  }
+  
+  // Maakt automatisch 42 <div> vakjes
+  makeBoard(event) {
+    event.preventDefault();
+    this.board.textContent = "";
+    for (let i = 0; i < 42; i++) {
+      this.hole = document.createElement("div");
+      this.hole.className = "item";
+      this.hole.setAttribute("id", i);
+      this.board.appendChild(this.hole);
+    }
+  }
+  hideStartWindow() {
+    this.start.classList.toggle("hidden");
+    this.playingField.classList.toggle("hidden");
+  }
+  hidePlayingField() {
+    this.winsScore1.textContent = 0;
+    this.winsScore2.textContent = 0;
+
+    this.playingField.classList.toggle("hidden");
+    this.start.classList.toggle("hidden");
+    this.statBlock1.classList.remove("player-one-turn");
+    this.statBlock2.classList.remove("player-two-turn");
+    this.stop.classList.toggle("inactive");
+    this.board.classList.toggle("inactive");
+  }
+  getNames() {
+    return [this.formName1.value, this.formName2.value];
+  }
+  getDifficulty() {
+    return this.difficulty.value;
+  }
+  getGamemode() { 
+    return this.gamemode.value;
+  }
+  setNames(players) {
+    this.scoreName1.textContent = players[0];
+    this.scoreName2.textContent = players[1];
+  }
+  updateTurn(turn) {
+    this.turn.textContent = turn;
+    this.statBlock1.classList.toggle("player-one-turn");
+    this.statBlock2.classList.toggle("player-two-turn");
+  }
+  async decideFirst(players, turn) {
+    let interval = 10;
+    let count = 0;
+    //ik voeg dit toe zodat het de goede schaduw meegeeft
+    this.statBlock2.classList.toggle("player-two-turn");
+    return new Promise((resolve) => {
+      const switcher = () => {
+        if (count < 15) {
+          if (count % 2 === 0) {
+            this.turn.textContent = players[0];
+          } else {
+            this.turn.textContent = players[1];
+          }
+          this.statBlock1.classList.toggle("player-one-turn");
+          this.statBlock2.classList.toggle("player-two-turn");
+          count++;
+          interval += 20;
+          setTimeout(switcher, interval);
+        } else {
+          //geef de ECHTE turn mee
+          if (players[0] === turn) {
+            this.turn.textContent = players[0];
+            this.statBlock1.classList.add("player-one-turn");
+            this.statBlock2.classList.remove("player-two-turn");
+          } else {
+            this.turn.textContent = players[1];
+            this.statBlock1.classList.remove("player-one-turn");
+            this.statBlock2.classList.add("player-two-turn");
+          }
+          this.stop.classList.toggle("inactive");
+          this.board.classList.toggle("inactive");
+          resolve();
+        }
+      };
+      setTimeout(switcher, interval);
+    });
+  }
+
+  placeChip(id, color) {
+    document.getElementById(id).style.backgroundColor = color;
+  }
+  endGame(winner) {
+    if (winner === null) {
+      this.winner.textContent = "Niemand heeft gewonnen...";
+    }
+    else {
+      this.winner.textContent = winner + " heeft gewonnen!";
+    }
+    this.end.classList.toggle("hidden");
+    this.toggleInteractivity();
+  }
+  updateWins(stats) {
+    document.getElementById(stats[1]).textContent = stats[0];
+  }
+  hideResultsWindow() {
+    this.end.classList.toggle("hidden");
+  }
+  toggleTitle() {
+    this.title.classList.toggle("hidden");
+  }
+  resetNames() {
+    this.formName1.value = "";
+    this.formName2.value = "";
+  } 
+  toggleInteractivity() {
+    this.board.classList.toggle("inactive");
+    this.stop.classList.toggle("inactive");
+  }
+}
