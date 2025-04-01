3254ef165cfe6d33d8f9594aa9901f2166564a75
JordiPan <jordipan.jp@gmail.com>
2025-04-01 15:57:31 +0200

    templates for injected html seperated from startmenu and made switch case in click eventlistener

--- startmenu.js
+++ startmenu.js
@@ -1,54 +1,80 @@
 import { OnlineGameHandler } from "./websockets/OnlineGameHandler.js";
+import { Templates } from "./templates/templates.js";
 // past niet echt in mvc model
 const startWindow = document.getElementById("start-window");
 const regex = /^[^ ].+[^ ]$/;
 const backendUrl = "http://localhost:3000";
 const client = new OnlineGameHandler();
+const templates = new Templates();
 
-//TODO: maak buttons unclickable terwijl het wacht voor backend dingen ofzo?
 window.addEventListener("load", async function(){
     changeToOffline();
-    // await connect();
 }, false);
 
-//switch case mayhaps?
 startWindow.addEventListener("click", async (event) => {
   if(!event.target) {
     return;
   }
-  if (event.target.id === "back-button") {
-    client.disconnect();
-    changeToOffline();
-    return;
-  }
-  if (event.target.id === "online-button") {
-    await connect();
-    // changeToOnline();
-    return;
-  }
-  if (event.target.id === "create-button") {
-    const usernameField = document.getElementById("online-username");
-    if(isValidName(usernameField)) {
-      setOnlineUsername(usernameField.value);
-      createRoom();
-    }
-    return;
-  }
-  //TODO: terug naar username knop later (basically disconnect)
-  if (event.target.id === "find-button") {
-    const usernameField = document.getElementById("online-username");
-    if(isValidName(usernameField)) {
-      setOnlineUsername(usernameField.value);
-      const rooms = await getRooms();
-      showRooms(rooms);
-    } 
-  }
 
-  if (event.target.id === "exit-waiting-button") {
-    await client.leaveRoom();
-    changeToOnline();
-    return;
+  switch(event.target.id) {
+    case "back-button": {
+      client.disconnect();
+      changeToOffline();
+      break;
+    }
+
+    case "online-button": {
+      await connect();
+      break;
+    }
+    
+    case "create-button": {
+      const usernameField = document.getElementById("online-username");
+      if(isValidName(usernameField)) {
+        setOnlineUsername(usernameField.value);
+        createRoom();
+      }
+      break;
+    }
+
+    case "find-button": {
+      const usernameField = document.getElementById("online-username");
+      if(isValidName(usernameField)) {
+        setOnlineUsername(usernameField.value);
+        const rooms = await getRooms();
+        showRooms(rooms);
+      }
+      break;
+    }
+
+    case "exit-waiting-button": {
+      await client.leaveRoom();
+      changeToOnline();
+      break;
+    }
+    default: {}
   }
+  // if (event.target.id === "back-button") {
+    
+  //   return;
+  // }
+  // if (event.target.id === "online-button") {
+  //   await connect();
+  //   // changeToOnline();
+  //   return;
+  // }
+  // if (event.target.id === "create-button") {
+    
+  //   return;
+  // }
+  // //TODO: terug naar username knop later (basically disconnect)
+  // if (event.target.id === "find-button") {
+  // }
+
+  // if (event.target.id === "exit-waiting-button") {
+    
+  //   return;
+  // }
 })
 
 startWindow.addEventListener("change", (event) => {
@@ -87,139 +113,28 @@ function isValidName(usernameField) {
 }
 
 function changeToOffline() {
-  startWindow.innerHTML = `<h2>Offline modus</h2>
-<div class="row mb-3">
-  <div class="col-md-6">
-    <label for="gamemode" class="form-label">Selecteer modus</label>
-    <select
-      type="text"
-      class="form-select"
-      id="gamemode"
-      name="gamemode"
-      required
-    >
-      <option selected value="">Kies een optie!!</option>
-      <option value="local">Lokaal</option>
-      <option value="ai">AI</option>
-    </select>
-  </div>
-  <div class="col-md-6">
-    <label for="difficulty" class="form-label">Moeilijkheid (AI)</label>
-    <select
-      type="text"
-      class="form-select"
-      id="difficulty"
-      name="gamemode"
-      disabled
-    >
-      <option selected value="easy">Makkelijk</option>
-      <option value="medium">Minder makkelijk</option>
-    </select>
-  </div>
-</div>
-
-<div class="mb-3 player1-field" id="player1-field">
-  <label for="speler-naam-1" class="form-label">Speler 1:</label>
-  <input
-    type="text"
-    class="form-control"
-    id="player1-name"
-    placeholder="John"
-    maxlength="15"
-    required
-    disabled
-  />
-</div>
-
-<div class="mb-3 player2-field" id="player2-field">
-  <label for="speler-naam-2" class="form-label">Speler 2:</label>
-  <input
-    type="text"
-    class="form-control"
-    id="player2-name"
-    placeholder="Titor"
-    maxlength="15"
-    required
-    disabled
-  />
-</div>
-
-<button class="btn btn-success" id="start-button">Start spel!</button>
-
-<button id="online-button" type="button">
-  <img
-    src="./img/online-icon.svg"
-    alt="online-icon"
-    id="online-icon"
-    class="menu-icon icon"
-  />
-</button>
-`
+  startWindow.innerHTML = templates.getOfflineWindow();
 }
 
 function changeToOnline() {
-  startWindow.innerHTML = `<h2>Online multiplaya!!</h2>
-
-<div class="mb-3 player1-field" id="player1-field">
-  <label for="online-username" class="form-label">Gebruikersnaam:</label>
-  <input
-    type="text"
-    class="form-control"
-    id="online-username"
-    placeholder="Silence"
-    maxlength="15"
-    pattern="^[^ ].+[^ ]$"
-  />
-</div>
-
-<!-- betere design later? -->
-<div id="online-buttons">
-  <button class="btn btn-success" id="find-button" type="button">Zoek spellen!</button>
-  <button class="btn btn-info" id="create-button" type="button">Maak een kamer!</button>
-  <button class="btn btn-danger" id="back-button" type="button">
-    <img
-      src="./img/offline-icon.svg"
-      alt="back to offline"
-      class="icon menu-icon"
-    />
-  </button>
-</div>
-<div class="list-container">
-  <div id="rooms-list">
-  </div>
-</div>`
+  startWindow.innerHTML = templates.getOnlineWindow();
 }
 function changeToWaitingRoom() {
-  startWindow.innerHTML = `
-  <div class="waiting-container">
-    <p>Kamer: ${client.getRoomId()}</p>
-    <p>gebruikersnaam: ${client.getUsername()}</p>
-    <p>Wachten voor andere speler...</p>
-    <img
-    src="./img/loading-white.svg"
-    alt="loading"
-    class="icon loading-icon"
-  />
-    <button type="button" id="exit-waiting-button">terug</button>
-  </div>`
+  const roomId = client.getRoomId();
+  const username = client.getUsername();
+
+  startWindow.innerHTML = templates.getWaitingRoom(roomId, username);
 }
 
 function changeToError() {
-  startWindow.innerHTML = `<h2>Probleem met server...</h2>
-<button class="btn btn-danger" id="back-button" type="button">
-    <img
-      src="./img/offline-icon.svg"
-      alt="back to offline"
-      class="icon menu-icon"
-    />
-  </button>`;
+  startWindow.innerHTML = templates.getErrorWindow();
 }
 function showRooms(rooms) {
   const roomsList = document.getElementById("rooms-list");
   roomsList.innerHTML = ``;
   // console.log("print rooms");
   if (Object.keys(rooms).length === 0) {
-    roomsList.innerHTML = `Niemand wilt dit spel spelen....`
+    roomsList.innerHTML = templates.getNoPlayersError();
   return;
   }
 
@@ -227,23 +142,13 @@ function showRooms(rooms) {
   for(const roomId in rooms) {
     const roomFragment = document.createElement("div");
     roomFragment.classList.add("room");
-    roomFragment.innerHTML = `
-    <div class="row room-row">
-      <div class="col info-col">
-        <p class="room-id">Room: ${roomId}</p>
-        <p class="creator-name">Creator: ${rooms[roomId]?.players[0]?.username} (${rooms[roomId]?.creatorId})</p>
-        <p class="player-count">${rooms[roomId]?.players?.length}/2</p>
-      </div>
-      <div class="col join-button-col">
-        <button class="join-button">
-          <img
-            src="./img/enter-icon.svg"
-            alt="enter icon"
-            class="enter-icon icon"
-          />
-        </button>
-      </div>
-    </div>`
+
+    const creator = rooms[roomId]?.players[0]?.username;
+    const creatorId = rooms[roomId]?.creatorId;
+    const playerCount = rooms[roomId]?.players?.length;
+
+    roomFragment.innerHTML = templates.getRoom(roomId, creator, creatorId, playerCount);
+
     fragment.appendChild(roomFragment);
   }
   roomsList.appendChild(fragment);
@@ -252,22 +157,12 @@ function showRooms(rooms) {
 async function getRooms() {
   const roomsList = document.getElementById("rooms-list");
   showLoading(roomsList);
-  // roomsList.innerHTML = `<img
-  //   src="./img/loading.svg"
-  //   alt="loading"
-  //   class="icon loading-icon"
-  // />`;
 
   const rooms = await client.getRooms();
   return rooms;
 }
 
 async function connect() {
-  // startWindow.innerHTML=`<img
-  // src="./img/loading-white.svg"
-  // alt="loading"
-  // class="icon loading-icon"
-  // />`;
   showLoading(startWindow);
   const result = await client.initialize(backendUrl);
 
@@ -287,11 +182,7 @@ async function createRoom() {
 }
 
 function showLoading(target) {
-  target.innerHTML=`<img
-  src="./img/loading-white.svg"
-  alt="loading"
-  class="icon loading-icon"
-  />`;
+  target.innerHTML = templates.getLoadingIcon();
 }
 
 // function emptyWindow() {
---
+++ templates/Templates.js
@@ -0,0 +1,157 @@
+export class Templates {
+  getOfflineWindow() {
+    return `<h2>Offline modus</h2>
+<div class="row mb-3">
+  <div class="col-md-6">
+    <label for="gamemode" class="form-label">Selecteer modus</label>
+    <select
+      type="text"
+      class="form-select"
+      id="gamemode"
+      name="gamemode"
+      required
+    >
+      <option selected value="">Kies een optie!!</option>
+      <option value="local">Lokaal</option>
+      <option value="ai">AI</option>
+    </select>
+  </div>
+  <div class="col-md-6">
+    <label for="difficulty" class="form-label">Moeilijkheid (AI)</label>
+    <select
+      type="text"
+      class="form-select"
+      id="difficulty"
+      name="gamemode"
+      disabled
+    >
+      <option selected value="easy">Makkelijk</option>
+      <option value="medium">Minder makkelijk</option>
+    </select>
+  </div>
+</div>
+
+<div class="mb-3 player1-field" id="player1-field">
+  <label for="speler-naam-1" class="form-label">Speler 1:</label>
+  <input
+    type="text"
+    class="form-control"
+    id="player1-name"
+    placeholder="John"
+    maxlength="15"
+    required
+    disabled
+  />
+</div>
+
+<div class="mb-3 player2-field" id="player2-field">
+  <label for="speler-naam-2" class="form-label">Speler 2:</label>
+  <input
+    type="text"
+    class="form-control"
+    id="player2-name"
+    placeholder="Titor"
+    maxlength="15"
+    required
+    disabled
+  />
+</div>
+
+<button class="btn btn-success" id="start-button">Start spel!</button>
+
+<button id="online-button" type="button">
+  <img
+    src="./img/online-icon.svg"
+    alt="online-icon"
+    id="online-icon"
+    class="menu-icon icon"
+  />
+</button>
+`;
+  }
+  getOnlineWindow() {
+    return `<h2>Online multiplaya!!</h2>
+
+<div class="mb-3 player1-field" id="player1-field">
+  <label for="online-username" class="form-label">Gebruikersnaam:</label>
+  <input
+    type="text"
+    class="form-control"
+    id="online-username"
+    placeholder="Silence"
+    maxlength="15"
+    pattern="^[^ ].+[^ ]$"
+  />
+</div>
+
+<!-- betere design later? -->
+<div id="online-buttons">
+  <button class="btn btn-success" id="find-button" type="button">Zoek spellen!</button>
+  <button class="btn btn-info" id="create-button" type="button">Maak een kamer!</button>
+  <button class="btn btn-danger" id="back-button" type="button">
+    <img
+      src="./img/offline-icon.svg"
+      alt="back to offline"
+      class="icon menu-icon"
+    />
+  </button>
+</div>
+<div class="list-container">
+  <div id="rooms-list">
+  </div>
+</div>`;
+  }
+  getWaitingRoom(roomId, username) {
+    return `
+  <div class="waiting-container">
+    <p>Kamer: ${roomId}</p>
+    <p>gebruikersnaam: ${username}</p>
+    <p>Wachten voor andere speler...</p>
+    <img
+    src="./img/loading-white.svg"
+    alt="loading"
+    class="icon loading-icon"
+  />
+    <button type="button" id="exit-waiting-button">terug</button>
+  </div>`;
+  }
+  getErrorWindow() {
+    `<h2>Probleem met server...</h2>
+<button class="btn btn-danger" id="back-button" type="button">
+    <img
+      src="./img/offline-icon.svg"
+      alt="back to offline"
+      class="icon menu-icon"
+    />
+  </button>`;
+  }
+  getNoPlayersError() {
+    return `Niemand wilt dit spel spelen....`;
+  }
+  getRoom(roomId, creator, creatorId, count) {
+    return `
+    <div class="row room-row">
+      <div class="col info-col">
+        <p class="room-id">Room: ${roomId}</p>
+        <p class="creator-name">Creator: ${creator} (${creatorId})</p>
+        <p class="player-count">${count}/2</p>
+      </div>
+      <div class="col join-button-col">
+        <button class="join-button" type="button">
+          <img
+            src="./img/enter-icon.svg"
+            alt="enter icon"
+            class="enter-icon icon"
+          />
+        </button>
+      </div>
+    </div>`;
+  }
+  getLoadingIcon() {
+    return `<img
+  src="./img/loading-white.svg"
+  alt="loading"
+  class="icon loading-icon"
+  />`;
+  }
+}
