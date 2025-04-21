7e8db835819badbcd0c91988eeee49e5a432e344
JordiPan <jordipan.jp@gmail.com>
2025-04-21 22:49:36 +0200

    idk

--- view/startmenu.js
+++ view/startmenu.js
@@ -2,7 +2,7 @@ import Client from "../websockets/OnlineGameHandler.js";
 import Templates from "../templates/Templates.js";
 import { Controller } from "../controller/Controller.js";
 import { OnlineController } from "../controller/OnlineController.js";
-import View from "../view/View.js";
+import View from "./View.js";
 
 export class StartMenu {
   constructor() {
